ad66305c4ed768f3b7ae4cf79dea24950a14f46b
Yurii Pochtiennykh <ukrwebprom@gmail.com>
2025-09-26 00:43:05 +0300

    upd
    upd

--- src/db.js
+++ src/db.js
@@ -2,11 +2,14 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  keepAlive: true,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 10000,
 });
 
 pool.on("error", (err) => {
   console.error("Unexpected PG error", err);
-  process.exit(1);
+  //process.exit(1);
 });
 
 const query = (text, params) => pool.query(text, params);
